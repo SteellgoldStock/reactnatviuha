214dbb93c25d62e73bcb33e198bc32b30f02e309
Gaëtan H <me@gaetanhus.fr>
2025-12-12 13:39:45 +0100

    reset

--- App.js
+++ App.js
@@ -1,49 +1,11 @@
-import { Button, Text, View } from 'react-native'
-import { useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
 
 export default function App() {
-  const [colors, setColors] = useState(['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF', '#000000' ])
-
-  const [num, setNum] = useState(160)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setColors((prev) => {
-        const newColors = [...prev]
-
-        for (let i = newColors.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1))
-          ;[newColors[i], newColors[j]] = [newColors[j], newColors[i]]
-        }
-
-        return newColors
-      })
-    }, num)
-
-    return () => clearInterval(interval)
-  }, [num])
-
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ flex: 2, backgroundColor: colors[0] }} />
-      <View style={{ flex: 2, backgroundColor: colors[1] }} />
-
-      <View style={{ flex: 1, flexDirection: "row", gap: 10, justifyContent: "center", alignItems: 'center' }}>
-        <Button title='-100' onPress={() => setNum(num - 100)} />
-        <Button title='-10' onPress={() => setNum(num - 10)} />
-        <Button title='-1' onPress={() => setNum(num - 1)} />
-
-        <Text style={{ fontSize: 32 }} onPress={() => setNum(160)}>
-          {num}
-        </Text>
-
-        <Button title='+1' onPress={() => setNum(num + 1)} />
-        <Button title='+10' onPress={() => setNum(num + 10)} />
-        <Button title='+100' onPress={() => setNum(num + 100)} />
-      </View>
-
-      <View style={{ flex: 2, backgroundColor: colors[2] }} />
-      <View style={{ flex: 2, backgroundColor: colors[3] }} />
-    </View>
+      <Text>
+        Hello World!
+      </Text>
+    </View> 
   )
 }
